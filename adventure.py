2e7e53cb8dec57d14527dc20d6c2ba69df525bed
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2019-02-20 19:32:03 -0600

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -5,5 +5,5 @@ print("Would you like instructions?")
 userInput = input("> ")
 if userInput == 'yes' or userInput == 'Yes' or userInput == 'YES':
     print("<INSTRUCTIONS GO HERE>")
-elif userInput == 'no' or userInput == 'No' userInput == 'NO':
+elif userInput == 'no' or userInput == 'No' or userInput == 'NO':
     print("<START GAME>")
