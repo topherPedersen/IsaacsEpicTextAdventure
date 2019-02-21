d8fa36191c3d959a0d405d612227114f068bda3d
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2019-02-20 19:30:37 -0600

    Create adventure.py

---
+++ adventure.py
@@ -0,0 +1,9 @@
+print("Welcome to Isaac's Epic Text Adventure!")
+print("")
+
+print("Would you like instructions?")
+userInput = input("> ")
+if userInput == 'yes' or userInput == 'Yes' or userInput == 'YES':
+    print("<INSTRUCTIONS GO HERE>")
+elif userInput == 'no' or userInput == 'No' userInput == 'NO':
+    print("<START GAME>")
