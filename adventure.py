1a3c36f920d4b89ba1aced9f1dff8f1cd3537435
Christopher Pedersen <12384786+topherPedersen@users.noreply.github.com>
2019-02-20 19:44:39 -0600

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -1,9 +1,19 @@
+# Isaacs Epic Text Adventure
+# Based on work by Willie Crowther & Don Woods (Colossal Cave Adventure, 1975-1977)
+
+# Declare Variables Before Game Start
+instructions = """
+To play the game, type short phrases into the command line below. If you type the
+word
+"""
+
+
 print("Welcome to Isaac's Epic Text Adventure!")
 print("")
 
 print("Would you like instructions?")
 userInput = input("> ")
 if userInput == 'yes' or userInput == 'Yes' or userInput == 'YES':
-    print("<INSTRUCTIONS GO HERE>")
+    print(instructions)
 elif userInput == 'no' or userInput == 'No' or userInput == 'NO':
     print("<START GAME>")
